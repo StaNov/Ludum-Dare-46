85b213616da08f5af82179dea5523ff0da4b65da
StaNov <info@stanov.cz>
2020-04-18 13:25:28 +0200

    Food & food spawner

---
+++ Ludum Dare 46/Assets/FoodSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject _foodPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_foodPrefab, new Vector2(3, 3), Quaternion.identity);
+    }
+}
