1c6e5b90e073369725b40ad774dc6351353152b0
StaNov <info@stanov.cz>
2020-04-18 16:26:00 +0200

    Coordinates

---
+++ Ludum Dare 46/Assets/Duplicator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Duplicator : MonoBehaviour
+{
+    public int count = 20;
+    public Transform numberTextPrefab;
+    void Start()
+    {
+        for (int i = 0; i < count; i++) {
+            Transform duplicate = Instantiate(numberTextPrefab, transform);
+            duplicate.GetComponent<Text>().text = (i + 1).ToString().PadLeft(2);
+        }
+    }
+}
