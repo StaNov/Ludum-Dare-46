ee61de26108a64d0a44d3e239158262fdd71855f
StaNov <info@stanov.cz>
2020-04-18 13:13:57 +0200

    Sample scene & guy physics

---
+++ Ludum Dare 46/Assets/GuyMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyMover : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _rigidbody2D.AddForce(new Vector2(1, 1) * 1.00001f, ForceMode2D.Impulse);
+    }
+}
