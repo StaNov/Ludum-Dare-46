79191cca87be78fe6218bce135483c8faeee9eb9
StaNov <info@stanov.cz>
2020-04-18 14:30:08 +0200

    Added twitch client

---
+++ Ludum Dare 46/Assets/TwitchChatClient.cs
@@ -0,0 +1,59 @@
+﻿using TwitchLib.Client.Models;
+using TwitchLib.Unity;
+using UnityEngine;
+
+public class TwitchChatClient : MonoBehaviour
+{
+	[SerializeField] //[SerializeField] Allows the private field to show up in Unity's inspector. Way better than just making it public
+	private string _channelToConnectTo = Secrets.USERNAME_FROM_OAUTH_TOKEN;
+
+	private Client _client;
+
+	private void Start()
+	{
+		ConnectionCredentials credentials = new ConnectionCredentials(Secrets.USERNAME_FROM_OAUTH_TOKEN, Secrets.OAUTH_TOKEN);
+
+		_client = new Client();
+		_client.Initialize(credentials, _channelToConnectTo);
+		_client.OnConnected += OnConnected;
+		_client.OnJoinedChannel += OnJoinedChannel;
+		_client.OnMessageReceived += OnMessageReceived;
+		_client.OnChatCommandReceived += OnChatCommandReceived;
+		_client.Connect();
+	}
+
+	private void OnConnected(object sender, TwitchLib.Client.Events.OnConnectedArgs e)
+	{
+		Debug.Log($"The bot {e.BotUsername} succesfully connected to Twitch.");
+
+		if (!string.IsNullOrWhiteSpace(e.AutoJoinChannel))
+			Debug.Log($"The bot will now attempt to automatically join the channel provided when the Initialize method was called: {e.AutoJoinChannel}");
+	}
+
+	private void OnJoinedChannel(object sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
+	{
+		Debug.Log($"The bot {e.BotUsername} just joined the channel: {e.Channel}");
+		_client.SendMessage(e.Channel, "I just joined the channel! PogChamp");
+	}
+
+	private void OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+	{
+		Debug.Log($"Message received from {e.ChatMessage.Username}: {e.ChatMessage.Message}");
+	}
+
+	private void OnChatCommandReceived(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
+	{
+		switch (e.Command.CommandText)
+		{
+			case "hello":
+				_client.SendMessage(e.Command.ChatMessage.Channel, $"Hello {e.Command.ChatMessage.DisplayName}!");
+				break;
+			case "about":
+				_client.SendMessage(e.Command.ChatMessage.Channel, "I am a Twitch bot running on TwitchLib!");
+				break;
+			default:
+				_client.SendMessage(e.Command.ChatMessage.Channel, $"Unknown chat command: {e.Command.CommandIdentifier}{e.Command.CommandText}");
+				break;
+		}
+	}
+}
