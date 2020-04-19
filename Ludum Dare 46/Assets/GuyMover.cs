4eca8bea88a1ccb54ebfd7bc49f8cb93bec59027
StaNov <info@stanov.cz>
2020-04-19 22:12:08 +0200

    Guy rotating

--- Ludum Dare 46/Assets/GuyMover.cs
+++ Ludum Dare 46/Assets/GuyMover.cs
@@ -3,15 +3,22 @@
 public class GuyMover : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
         _rigidbody2D.AddForce(new Vector2(RandomDirection(), RandomDirection()), ForceMode2D.Impulse);
+    }
+
+    void LateUpdate()
+    {
+        _spriteRenderer.flipX = _rigidbody2D.velocity.x < 0;
     }
 
     public void IncreaseVelocity()
