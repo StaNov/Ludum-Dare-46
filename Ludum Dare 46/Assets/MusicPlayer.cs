388c2964457ce48698a2c0669a74ea28a2014bf7
StaNov <info@stanov.cz>
2020-04-19 22:55:28 +0200

    Food plops

--- Ludum Dare 46/Assets/MusicPlayer.cs
+++ Ludum Dare 46/Assets/MusicPlayer.cs
@@ -3,14 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MusicPlayer : MonoBehaviour
 {
     public static MusicPlayer _instance;
 
+    public AudioClip[] foodPlops;
+
     private AudioSource _kachon;
     private AudioSource _kytara;
     private AudioSource _kazoo;
+    private AudioSource _effects;
 
 
     void Awake()
@@ -32,10 +36,17 @@ public class MusicPlayer : MonoBehaviour
         _kachon = audioSources[0];
         _kytara = audioSources[1];
         _kazoo = audioSources[2];
+        _effects = audioSources[3];
+        
 
         foreach (var audioSource in audioSources)
         {
             audioSource.Play();
         }
+    }
+
+    public void PlayFoodPlop()
+    {
+        _effects.PlayOneShot(foodPlops[Random.Range(0, foodPlops.Length - 1)]);
     }
 }
