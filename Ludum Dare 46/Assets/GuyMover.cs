4471089db0c63d242466e93f53815a2a0e1db684
StaNov <info@stanov.cz>
2020-04-19 15:07:46 +0200

    Randomized starting direction

--- Ludum Dare 46/Assets/GuyMover.cs
+++ Ludum Dare 46/Assets/GuyMover.cs
@@ -11,11 +11,23 @@ public class GuyMover : MonoBehaviour
 
     void Start()
     {
-        _rigidbody2D.AddForce(new Vector2(1, 1) * 1.00001f, ForceMode2D.Impulse);
+        _rigidbody2D.AddForce(new Vector2(RandomDirection(), RandomDirection()).normalized * 1.5f, ForceMode2D.Impulse);
     }
 
     public void IncreaseVelocity()
     {
         _rigidbody2D.velocity = _rigidbody2D.velocity * 1.1f;
+    }
+
+    private float RandomDirection()
+    {
+        float number = Random.Range(0.9f, 1.1f);
+
+        if (Random.Range(0f, 1f) > 0.5f)
+        {
+            number *= -1;
+        }
+
+        return number;
     }
 }
