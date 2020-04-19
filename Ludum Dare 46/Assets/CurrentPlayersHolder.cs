857af686b0b0d1272cf605596965b248923a9f45
StaNov <info@stanov.cz>
2020-04-19 12:21:51 +0200

    Current players showing

---
+++ Ludum Dare 46/Assets/CurrentPlayersHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TwitchLib.Client.Events;
+using UnityEngine;
+
+public class CurrentPlayersHolder : MonoBehaviour
+{
+    private TwitchChatClient _twitchChatClient;
+    public DebugInput debugInput;
+    public List<string> currentPlayers = new List<string>();
+    
+    void Awake()
+    {
+        #if !NOT_TWITCH
+        _twitchChatClient = TwitchChatClient.Instance;
+        _twitchChatClient.AddOnCommandReceived(OnCommand);
+        #endif
+    }
+    
+    #if !NOT_TWITCH
+    private void OnDestroy()
+    {
+        _twitchChatClient.RemoveOnCommandReceived(OnCommand);
+    }
+    #endif
+
+    private void OnCommand(object sender, OnChatCommandReceivedArgs command)
+    {
+        OnCommand(command);
+    }
+
+    private void OnCommand(OnChatCommandReceivedArgs command)
+    {
+        string userName = command.Command.ChatMessage.Username;
+        if (!currentPlayers.Contains(userName))
+        {
+            currentPlayers.Add(userName);
+        }
+    }
+}
---
+++ Ludum Dare 46/Assets/CurrentPlayersText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentPlayersText : MonoBehaviour
+{
+    public CurrentPlayersHolder currentPlayersHolder;
+    private Text _text;
+    void Awake()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        string newText = string.Join("\n", currentPlayersHolder.currentPlayers);
+
+        if (!_text.text.Equals(newText))
+        {
+            _text.text = newText;
+        }
+    }
+}
